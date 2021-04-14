3e37058d10c81a460352bd71a944c44b2d68b229
wangxingkang <wang_xingkang@qq.com>
2021-04-14 11:57:23 +0800

    📝 docs(split-screen): 完善示例

--- packages/split-screen/docs/demo/demo-02.tsx
+++ packages/split-screen/docs/demo/demo-02.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { Select, Space } from 'antd';
 import SplitScreen from '@pansy/react-split-screen';
+import type { SplitScreenAmount } from '@pansy/react-split-screen/es/types';
 import Player from '@pansy/react-aliplayer';
 
-const list = [1, 4, 6, 8, 9, 13 , 16];
+const list: SplitScreenAmount[] = [1, 4, 6, 8, 9, 13, 16];
 
 const options = list.map(item => ({ value: item, label: item }))
 
 export default () => {
-  const [current, setCurrent] = useState<number>(4);
+  const [current, setCurrent] = useState<SplitScreenAmount>(4);
 
   return (
     <Space direction="vertical" style={{ width: '100%'}}>
@@ -25,7 +26,8 @@ export default () => {
               <Player
                 source="//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4"
                 options={{
-                  autoplay: true
+                  autoplay: true,
+                  rePlay: true,
                 }}
               />
             )
